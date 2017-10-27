1d8588eaf99397c7f312017a09d97f24f56e726c
JordyAlkema <me@jordyalkema.nl>
2017-10-27 16:44:43 +0200

    final commit (met een beetje geluk)

--- init.py
+++ init.py
@@ -1,11 +1,8 @@
 import configparser;
-import requests;
-from socketIO_client import SocketIO
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from classes import Client
 from classes import Server
-from classes.sensors import Button
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -15,7 +12,10 @@ client = Client.Client(config, server)
 
 scheduler = BackgroundScheduler()
 
+# for detecting button input
 alarmSwitch = scheduler.add_job(client.alarmButton, 'interval', seconds=0.1)
+
+# for watching for socket events
 server.socket.on('alarm', client.serverAlarmRequest)
 server.socket.on('disconnect', client.connectionLost)
 server.socket.on('reconnect', client.reconnect)
